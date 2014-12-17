20ea8f8d7fd299c79fb866b05040a6c33ce3b5ce
AaronPlave <aplave@wesleyan.edu>
2014-12-17 17:35:25 -0500

    blergh

--- runserver.py
+++ runserver.py
@@ -1,2 +1,10 @@
-from server import app
-app.run(debug=True, host='0.0.0.0')
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
